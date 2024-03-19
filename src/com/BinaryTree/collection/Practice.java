68325ef0959b2bae73b8f3dc48c900e77da58e75
akbarshaikh1627 <akshaikh27@gmail.com>
2024-03-19 15:13:47 +0530

    third commit

--- src/com/BinaryTree/collection/Practice.java
+++ src/com/BinaryTree/collection/Practice.java
@@ -7,5 +7,15 @@ public class Practice
     public static void main(String[] args)
     {
        System.out.println("hello java ..");
+       
+       int x=10;
+       if(x%2==0)
+       {
+    	   System.out.println("its prime number");
+       }
+       else
+       {
+    	   System.out.println("its even number");
+       }
     }
 }
